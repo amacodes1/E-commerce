407b3ac35177b84b1da89a5995f0e0bdd5473321
mercyama <mercyezihe2@gmail.com>
2023-03-31 15:13:40 +0100

    committed an update

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const product = document.getElementById("product");
+const product = document.querySelector(".row");
 
 let data = [
     {
         id: 1,
         name1: "Green Bag",
-        price: 5500,
+        price: 550,
         description: "Beautiful ladies green bag",
+        image: "images/green-bag.jpeg"
     },
     {
         id: 2,
         name1: "Sneakers",
-        price: 7000,
+        price: 700,
         description: "Unisex Sneakers",
+        image: "images/sneakers.jpg"
     },
     {
         id: 3,
         name1: "Crocs",
-        price: 5000,
+        price: 500,
         description: "High female crocs",
+        image: "images/crocs.webp"
     },
     {
         id: 4,
         name1: "Belt",
-        price: 3000,
+        price: 300,
         description: "High quality belt",
+        image: "images/belt.jpeg"
+    },
+    {
+        id: 4,
+        name1: "Belt",
+        price: 400,
+        description: "High quality belt",
+        image: "images/belt.jpeg"
+    },
+    {
+        id: 4,
+        name1: "Belt",
+        price: 150,
+        description: "High quality belt",
+        image: "images/belt.jpeg"
     }
 ]
 
@@ -31,12 +49,12 @@ let dataList = ""
 for (let i = 0; i < data.length; i++){
 
     dataList += `
-    <div class="col-6">
+    <div class="col-lg-4 col-sm-12">
     <div class="card" style="width: 18rem;">
-        <img src="images/green-bag.jpeg" class="card-img-top" alt="...">
+        <img src="${data[i].image}" class="card-img-top" alt="...">
         <div class="card-body">
            <h5 class="card-title">${data[i].name1}</h5>
-           <p class="card-text">${data[i].price}</p>
+           <p class="card-text">$${data[i].price}</p>
            <p class="card-text">${data[i].description}</p>
            <a href="#" class="btn btn-primary">Add to cart</a>
            <a href="#" class="btn btn-warning">Buy Now</a>
@@ -47,9 +65,3 @@ for (let i = 0; i < data.length; i++){
    product.innerHTML = dataList;
 }
 
-<ul>
-      <li>${data[i].id}</li>
-      <li>${data[i].name1}</li>
-      <li>${data[i].price}</li>
-      <li>${data[i].description}</li>
-   </ul>
